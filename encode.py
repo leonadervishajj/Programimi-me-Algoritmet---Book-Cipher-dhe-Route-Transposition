590fb15b39ab56806c23fdc985121dbee113832b
leonadervishajj <leona.dervishaj@student.uni-pr.edu>
2026-04-13 15:04:16 +0200

    Ristrukturimi i funksionit të enkriptimit për performancë më të mirë

--- encode.py
+++ encode.py
@@ -1,12 +1,15 @@
-
 def book_cipher_encode(text, book_words):
     result = []
+    book_lower = [w.lower() for w in book_words]
+    
     words = text.lower().split()
 
     for word in words:
-        if word in book_words:
-            result.append(str(book_words.index(word)))
+        if word in book_lower:
+         
+            pozicioni = book_lower.index(word)
+            result.append(str(pozicioni))
         else:
             result.append("?")
-
-    return " ".join(result)    # Ky modul implementon funksionin per enkriptimin e mesazheve duke perdorur Book Cipher
+            
+    return " ".join(result)
