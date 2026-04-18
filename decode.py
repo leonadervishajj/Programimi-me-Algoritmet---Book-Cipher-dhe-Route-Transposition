9a0c082a5b470b5bcfecff3b09ef6f9e3885d1f2
leonadervishajj <leona.dervishaj@student.uni-pr.edu>
2026-04-18 20:00:50 +0200

    U kompletua Book Cipher (Encode dhe Decode)

--- decode.py
+++ decode.py
@@ -1,15 +1,18 @@
 def book_cipher_decode(encoded_text, book_words):
     result = []
-    tokens = encoded_text.split() 
-    
+    tokens = encoded_text.split()
+
     for token in tokens:
         if token == "?":
             result.append("?")
         else:
             try:
                 index = int(token)
-                result.append(book_words[index])
+                if 0 <= index < len(book_words):
+                    result.append(book_words[index])
+                else:
+                    result.append("?")
             except (ValueError, IndexError):
                 result.append("?")
-    
+
     return " ".join(result)
